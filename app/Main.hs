e2720570bd062b4b55fbec5de3a61cd47a739611
guillaume <guillaume.ridey94@gmail.com>
2020-04-01 11:11:27 +0200

    first commit

--- app/Main.hs
+++ app/Main.hs
@@ -29,13 +29,17 @@ import qualified Keyboard as K
 
 import qualified Debug.Trace as T
 
-import Model (GameState)
+import Data.Map as Map
+
+import Model (Carte)
+import Model (Coord)
 import qualified Model as M
+import System.Random as R
 
 loadBackground :: Renderer-> FilePath -> TextureMap -> SpriteMap -> IO (TextureMap, SpriteMap)
 loadBackground rdr path tmap smap = do
   tmap' <- TM.loadTexture rdr path (TextureId "background") tmap
-  let sprite = S.defaultScale $ S.addImage S.createEmptySprite $ S.createImage (TextureId "background") (S.mkArea 0 0 640 480)
+  let sprite = S.defaultScale $ S.addImage S.createEmptySprite $ S.createImage (TextureId "background") (S.mkArea 0 0 1280 720)
   let smap' = SM.addSprite (SpriteId "background") sprite smap
   return (tmap', smap')
 
@@ -48,42 +52,68 @@ loadPerso rdr path tmap smap = do
 
 loadVirus :: Renderer-> FilePath -> TextureMap -> SpriteMap -> IO (TextureMap, SpriteMap)
 loadVirus rdr path tmap smap = do
-  tmap' <- TM.loadTexture rdr path (TextureId "virus") tmap
-  let sprite = S.defaultScale $ S.addImage S.createEmptySprite $ S.createImage (TextureId "virus") (S.mkArea 0 0 100 100)
-  let smap' = SM.addSprite (SpriteId "virus") sprite smap
+  tmap' <- TM.loadTexture rdr path (TextureId "mur") tmap
+  let sprite = S.defaultScale $ S.addImage S.createEmptySprite $ S.createImage (TextureId "mur") (S.mkArea 0 0 50 50)
+  let smap' = SM.addSprite (SpriteId "mur") sprite smap
   return (tmap', smap')
 
+
+loadSol :: Renderer-> FilePath -> TextureMap -> SpriteMap -> IO (TextureMap, SpriteMap)
+loadSol rdr path tmap smap = do
+  tmap' <- TM.loadTexture rdr path (TextureId "sol") tmap
+  let sprite = S.defaultScale $ S.addImage S.createEmptySprite $ S.createImage (TextureId "sol") (S.mkArea 0 0 100 100)
+  let smap' = SM.addSprite (SpriteId "sol") sprite smap
+  return (tmap', smap')
 
 
 
 main :: IO ()
 main = do
   initializeAll
-  window <- createWindow "Minijeu" $ defaultWindow { windowInitialSize = V2 640 480 }
+  window <- createWindow "Minijeu" $ defaultWindow { windowInitialSize = V2 1280 720 }
   renderer <- createRenderer window (-1) defaultRenderer
   -- chargement de l'image du fond
   (tmap, smap) <- loadBackground renderer "assets/background.jpg" TM.createTextureMap SM.createSpriteMap
   -- chargement du personnage
   (tmap', smap') <- loadPerso renderer "assets/perso.png" tmap smap
   -- chargement du virus
-  (tmap'', smap'') <- loadVirus renderer "assets/virus.png" tmap' smap'
-  
+  (tmap'', smap'') <- loadVirus renderer "assets/angle_wall2.png" tmap' smap'
+  (tmap3, smap3) <- loadSol renderer "assets/sol.png" tmap'' smap''
+  x <- randomRIO (0 ,540)
+  y <- randomRIO (0 ,380)
   -- initialisation de l'état du jeu
-  let gameState = M.initGameState
+  map <- readFile "assets/map1.txt"
+  let carte = M.initGameState x y map
   -- initialisation de l'état du clavier
   let kbd = K.createKeyboard
   -- lancement de la gameLoop
-  gameLoop 60 renderer tmap'' smap'' kbd gameState
+  gameLoop 60 renderer tmap3 smap3 kbd carte
 
-gameLoop :: (RealFrac a, Show a) => a -> Renderer -> TextureMap -> SpriteMap -> Keyboard -> GameState -> IO ()
-gameLoop frameRate renderer tmap smap kbd gameState = do
+  --print (M.carte_contenu carte) 
+
+
+affichage ::Coord -> M.Case -> SpriteMap ->Sprite
+affichage (M.C x y) cases smap= (S.moveTo (SM.fetchSprite (SpriteId (M.caseToName cases)) smap)
+                                 (fromIntegral x)
+                                 (fromIntegral y))
+
+
+affichageMap :: [(M.Coord,M.Case)] -> Renderer -> TextureMap -> SpriteMap -> IO ()
+affichageMap ((coord,cases):[]) renderer tmap smap = S.displaySprite renderer tmap (affichage coord cases smap)
+affichageMap ((coord,cases):tail) renderer tmap smap =do
+  S.displaySprite renderer tmap (affichage coord cases smap)
+  affichageMap tail renderer tmap smap
+
+gameLoop :: (RealFrac a, Show a) => a -> Renderer -> TextureMap -> SpriteMap -> Keyboard -> Carte -> IO ()
+gameLoop frameRate renderer tmap smap kbd carte = do
   startTime <- time
   events <- pollEvents
   let kbd' = K.handleEvents events kbd
   clear renderer
+
   --- display background
   S.displaySprite renderer tmap (SM.fetchSprite (SpriteId "background") smap)
-  --- display perso 
+  {--- display perso 
   S.displaySprite renderer tmap (S.moveTo (SM.fetchSprite (SpriteId "perso") smap)
                                  (fromIntegral (M.persoX gameState))
                                  (fromIntegral (M.persoY gameState)))
@@ -91,16 +121,18 @@ gameLoop frameRate renderer tmap smap kbd gameState = do
   S.displaySprite renderer tmap (S.moveTo (SM.fetchSprite (SpriteId "virus") smap)
                                  (fromIntegral (M.virusX gameState))
                                  (fromIntegral (M.virusY gameState)))
+  -}
+  affichageMap (Map.toList (M.carte_contenu carte)) renderer tmap smap 
   present renderer
-  endTime <- time
-  let refreshTime = endTime - startTime
-  let delayTime = floor (((1.0 / frameRate) - refreshTime) * 1000)
-  threadDelay $ delayTime * 1000 -- microseconds
-  endTime <- time
-  let deltaTime = endTime - startTime
+  --endTime <- time
+  --let refreshTime = endTime - startTime
+  --let delayTime = floor (((1.0 / frameRate) - refreshTime) * 1000)
+  --threadDelay $ delayTime * 1000 -- microseconds
+  --endTime <- time
+  --let deltaTime = endTime - startTime
   -- putStrLn $ "Delta time: " <> (show (deltaTime * 1000)) <> " (ms)"
   -- putStrLn $ "Frame rate: " <> (show (1 / deltaTime)) <> " (frame/s)"
   --- update du game state
-  let gameState' = M.gameStep gameState kbd' deltaTime
+  --let gameState' = M.gameStep gameState kbd' deltaTime
   ---
-  unless (K.keypressed KeycodeEscape kbd') (gameLoop frameRate renderer tmap smap kbd' gameState')
+  unless (K.keypressed KeycodeEscape kbd') (gameLoop frameRate renderer tmap smap kbd' carte)
--- src/Model.hs
+++ src/Model.hs
@@ -7,23 +7,59 @@ import Keyboard (Keyboard)
 import qualified Keyboard as K
 import System.Random as R
 import System.IO.Unsafe
+import Data.Map as M
 
-data GameState = GameState { persoX :: Int
-                           , persoY :: Int
-                           , virusX :: Int
-                           , virusY :: Int
-                           , speed :: Int }
-  deriving (Show)
+data Case = Normal -- une case vide
+  | Perso --id
+  | Mur -- infranchissable (sauf pour les fantomes ...)
+  deriving (Eq,Show)
+
+data Coord = C {cx :: Int , cy :: Int} deriving (Eq,Show)
+
+instance Ord Coord where
+  (C x1 y1) <= (C x2 y2) = if y1 < y2 then True else if y1 > y2 then False else x1 <= x2
+
+--instance Show Case where
+--show x = "mur"
+
+data Carte = Carte { cartel :: Int , -- largeur
+                     carteh :: Int , -- hauteur
+                     carte_contenu :: (M.Map Coord Case) -- cases de la carte 
+                     }
+ -- deriving (Show Carte)
 
 
-randomInt :: Int -> Int -> Int
-randomInt min max = unsafePerformIO (R.getStdRandom (R.randomR (min, max)))
+--randomInt :: Int -> Int -> Int
+--randomInt min max = unsafePerformIO (R.getStdRandom (R.randomR (min, max)))
+
+--initGameState :: GameState
+--initGameState = GameState 200 300 (randomInt 0 540) (randomInt 0 380) 4
 
 
 
-initGameState :: GameState
-initGameState = GameState 200 300 (randomInt 0 540) (randomInt 0 380) 4
+associate :: Char -> Case
+associate x = case x of
+  '*' -> Mur
+  ' ' -> Model.Normal
+  'X' -> Perso
+  otherwise -> Mur
+caseToName :: Case -> String
+caseToName x = case x of
+   Mur -> "mur"
+   Model.Normal -> "sol"
+   Perso -> "perso"
 
+initCarte :: String -> Coord -> M.Map Coord Case -> M.Map Coord Case
+initCarte [] _ acc = acc
+initCarte (head:tail) (C x y) acc = if head == '\n' then initCarte tail (C 0 (y+50)) acc else initCarte tail (C (x+50) y) (M.insert (C x y) (associate head) acc)
+
+
+initGameState :: Int -> Int -> String -> Carte
+initGameState x y entre = Carte 200 300 (initCarte entre (C 0 0) M.empty)
+
+
+
+{-
 moveLeft :: GameState -> GameState
 moveLeft gs@(GameState px _ _ _ sp) | px > 0 = gs { persoX = px - sp }
                                 | otherwise = gs
@@ -70,3 +106,4 @@ moveTo gstate KeycodeS deltaTime = let newG = moveDown gstate in if testTouch ne
 moveTo gstate KeycodeD deltaTime = let newG = moveRight gstate in if testTouch newG then moveVirus newG (-100) (-100) else newG
 moveTo gstate _ _ = gstate
 
+-}
